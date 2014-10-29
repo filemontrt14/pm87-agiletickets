eaa69be44f2581846a88f14651dc5224edcb36e2
Filemon de Castro Santos <filemon.santos@trt14.jus.br>
2014-10-29 09:45:10 -0400

    alteracoes da aula

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -6,14 +6,6 @@ import org.junit.Test;
 public class SessaoTest {
 
 	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
-
-		Assert.assertTrue(sessao.podeReservar(1));
-	}
-
-	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
@@ -39,4 +31,11 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 
+	@Test
+	public void deveVender10ingressosSeHa10vagas()
+			throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(10);
+		Assert.assertTrue(sessao.podeReservar(10));
+	}
 }
